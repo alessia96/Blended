51708b3b384ef71fbd2d566a0442d9d6f2e5af0e
Alessia Nigretti <alessia.nigretti@gmail.com>
2017-02-04 15:58:18 +0000

    Added script on terminal

---
+++ blended/Assets/Scripts/Terminal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terminal : MonoBehaviour
+{
+    private bool active;
+
+    void Start()
+    {
+        active = true;
+	}
+	
+	void OnMouseDown()
+    {
+        if (active)
+        {
+            OpenTerminal();
+            active = false;
+        }
+    }
+
+    void OpenTerminal()
+    {
+        //govind's stuff
+        Debug.Log("opened terminal");
+    }
+}
