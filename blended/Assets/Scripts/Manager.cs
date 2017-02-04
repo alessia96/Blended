7a6f7c69ba13dd65af5593b3592f6ee901653ce3
govind <venkateshgovind1@gmail.com>
2017-02-04 20:24:08 +0000

    added type writer script and skeleton event manager

---
+++ blended/Assets/Scripts/Manager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+    private bool text;
+    private bool logoAnim = false;
+    GameObject curtainClose = GameObject.Find("CurtainClose");
+    GameObject curtainOpen = GameObject.Find("CurtainOpen");
+
+    // Use this for initialization
+    void Start () {
+        curtainOpen.SetActive(true);
+        text = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //get the key pressed
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            logoAnim = true;
+            //play the animation
+            if (logoAnim) {
+                //play logo animation and check for its finish using isPlaying()
+                //set curtainOpen to inactive and activate curtain close
+                if (!isPlaying()) {
+                    curtainClose.SetActive(true);
+                    curtainOpen.SetActive(false);
+                }
+            }
+            text = true;
+        }
+        if (text) {
+            //scroll text animation
+
+        }
+   
+	}
+    bool isPlaying(){
+        return true;
+    }
+}
