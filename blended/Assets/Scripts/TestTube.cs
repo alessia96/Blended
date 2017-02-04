c014cf8bf9d55eccbed8f08e5124f12af7ac99a9
Alessia Nigretti <alessia.nigretti@gmail.com>
2017-02-04 19:37:27 +0000

    Changed interaction with test tubes

---
+++ blended/Assets/Scripts/TestTube.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTube : MonoBehaviour
+{
+    private bool terminalInUse;
+    public GameObject terminal;
+    private Vector3 testTubeLocation; //saving location before animation
+
+    void OnMouseDown()
+    {
+        terminalInUse = terminal.GetComponent<Terminal>().GetActive();
+
+        if (!terminalInUse)
+        {
+            UseTestTube();
+        }
+    }
+
+    void UseTestTube()
+    {
+        Debug.Log("testtube in use");
+        testTubeLocation = this.GetComponent<Transform>().position;
+        this.GetComponent<Animator>().enabled = true;
+    }
+
+    void PutTestTubeBack()
+    {
+        Debug.Log("testtube is back");
+        this.GetComponent<Animator>().enabled = false;
+        this.GetComponent<Animation>().enabled = false;
+        this.transform.Translate(testTubeLocation);
+    }
+}
