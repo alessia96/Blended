b2b2203f5a70d9d37a002e8450b861849c44c787
Alessia Nigretti <alessia.nigretti@gmail.com>
2017-02-05 05:59:19 +0000

    Changes to main and start£

---
+++ blended/Assets/Scripts/video.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class video : MonoBehaviour {
+
+    public MovieTexture texture;
+    
+
+    // Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = texture as MovieTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playMovie();
+	}
+
+    void playMovie() {
+        texture.Play();
+    }
+}
