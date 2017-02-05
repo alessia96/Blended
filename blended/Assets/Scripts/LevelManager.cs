76a37beab390ad3623ddf208fe90ad7f0a24525e
Alessia Nigretti <alessia.nigretti@gmail.com>
2017-02-05 05:48:38 +0000

    Added level manager

---
+++ blended/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    private GameObject[] picks;
+
+    void Start()
+    {
+        picks = new GameObject[3];
+    }
+
+    void AddToPicked(GameObject testTube)
+    {
+        if (picks[0] == null)
+        {
+            picks[0] = testTube;
+        } else if (picks[1] == null)
+        {
+            picks[1] = testTube;
+        } else if (picks[2] == null)
+        {
+            picks[2] = testTube;
+        }
+    }
+	
+}
