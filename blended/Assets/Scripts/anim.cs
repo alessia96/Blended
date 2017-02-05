bdebb4d09a92239c919bfff65e490d1c565a9e43
govind <venkateshgovind1@gmail.com>
2017-02-05 11:08:55 +0000

    animated logo

--- blended/Assets/Scripts/anim.cs
+++ blended/Assets/Scripts/anim.cs
@@ -5,13 +5,7 @@ public class anim : MonoBehaviour {
     public GameObject logo;
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
         MovieTexture comp = (MovieTexture)logo.GetComponent<Renderer>().material.mainTexture;
         comp.Play();
-
-	}
+    }
 }
